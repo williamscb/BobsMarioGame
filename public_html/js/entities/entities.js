0a196ec9ece188450dcc5034b2d80fb15cadb47d
Christopher Williams <cwilks225@gmail.com>
2014-11-19 07:42:38 -0800

    6th commit

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -1,17 +1,24 @@
 // TODO
 game.PlayerEntity = me.Entity.extend({
-  init: function(x, y, settings) {
-      settings.image = "mario";
-      settings.spritewidth = "128";
-      settings.spriteheight = "128";
-      settings.width = 128;
-      settings.height = 128; 
-      this._super(me.Entity,'init',(x, y, settings));
+    init: function(x, y, settings) {
+        settings.image = "mario";
+        settings.spritewidth = "128";
+        settings.spriteheight = "128";
+        settings.width = 128;
+        settings.height = 128;
+        this._super(me.Entity, 'init', [x, y, settings]);
+
+        this.body.setVelocity(5, 0);
     },
-  
-      update: function(){
     
-}
-       
+    update: function() {
+        if (me.input.isKeyPressed("right")) {
+            this.body.vel.x += this.body.accel.x = me.timer.tick;
+        } else {
+            this.body.vel.x = 0;
+            //this
+        }
+    }
+
 });
         
--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -5,7 +5,7 @@ game.resources = [
 	 * {name: "example", type:"image", src: "data/img/example.png"},
 	 */
         {name: "background-tiles", type:"image", src: "data/img/background-tiles.png"},
-        {name: "mario", type:"image", src: "data/imgplayer1.png"},
+        {name: "mario", type:"image", src: "data/img/player1.png"},
 	/* Atlases 
 	 * @example
 	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
