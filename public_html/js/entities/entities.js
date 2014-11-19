92d12c1d93503f4bd791727a0035782d3a933501
Christopher Williams <cwilks225@gmail.com>
2014-11-19 13:55:00 -0800

    7th commit

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -1,6 +1,19 @@
 // TODO
 game.PlayerEntity = me.Entity.extend({
     init: function(x, y, settings) {
+     this._super(me.Entity, "init",x ,y );
+     image: "matio";
+     spritewidth; "128",
+     spriteheight; "128",
+     width;128,
+     height;128,
+        getShape: function(){
+            return (new.me.Rect(0, 0, 128, 128)).toPolygon();
+            
+        }
+    }]);
+
+        
         settings.image = "mario";
         settings.spritewidth = "128";
         settings.spriteheight = "128";
@@ -16,8 +29,10 @@ game.PlayerEntity = me.Entity.extend({
             this.body.vel.x += this.body.accel.x = me.timer.tick;
         } else {
             this.body.vel.x = 0;
-            //this
+            
         }
+        this.body.update(delta);
+        return true;
     }
 
 });
