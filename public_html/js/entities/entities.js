5b99de897791829b58ebfd5cc7e28b31fd021e6f
Christopher Williams <cwilks225@gmail.com>
2014-11-19 14:03:17 -0800

    8th commit

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -1,37 +1,34 @@
 // TODO
 game.PlayerEntity = me.Entity.extend({
-    init: function(x, y, settings) {
-     this._super(me.Entity, "init",x ,y );
-     image: "matio";
-     spritewidth; "128",
-     spriteheight; "128",
-     width;128,
-     height;128,
+init: function(x, y, settings) {
+this._super(me.Entity, "init", x, y);
+        image: "matio";
+        spritewidth; "128",
+        spriteheight; "128",
+        width; 128,
+        height; 128,
         getShape: function(){
-            return (new.me.Rect(0, 0, 128, 128)).toPolygon();
-            
-        }
-    }]);
+        return (new .me.Rect(0, 0, 128, 128))
 
-        
-        settings.image = "mario";
+        }
+
+
+settings.image = "mario";
         settings.spritewidth = "128";
         settings.spriteheight = "128";
         settings.width = 128;
         settings.height = 128;
         this._super(me.Entity, 'init', [x, y, settings]);
-
         this.body.setVelocity(5, 0);
-    },
-    
-    update: function() {
+},
+        update: function() {
         if (me.input.isKeyPressed("right")) {
-            this.body.vel.x += this.body.accel.x = me.timer.tick;
+        this.body.vel.x += this.body.accel.x = me.timer.tick;
         } else {
-            this.body.vel.x = 0;
-            
+        this.body.vel.x = 0;
         }
         this.body.update(delta);
+        this._super(me.Entity, "update", [delta]);
         return true;
     }
 
